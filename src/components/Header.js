31771e7cf203bb8812b9b715376a53f9ae427d6f
Joao Victor <imashinzo10@gmail.com>
2022-10-31 17:28:04 -0300

    Requisito 1 finalizado erros no console

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Header extends Component {
   render() {
+    const { email, expenses } = this.props;
+    let total = 0;
+    if (expenses.length !== 0) {
+      expenses.forEach((expense) => {
+        total += Number(expense.value * expense.exchangeRates[expense.currency].ask);
+      });
+    }
     return (
-      <div>Header</div>
+      <header>
+        <p data-testid="email-field">{ email }</p>
+        <p data-testid="total-field">{total.toFixed(2)}</p>
+        <p data-testid="header-currency-field">BRL</p>
+      </header>
     );
   }
 }
-
-export default Header;
