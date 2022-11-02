b4c8cba36121f3f7955ce71de8aa544d538474ff
Joao Victor <imashinzo10@gmail.com>
2022-11-02 20:54:40 -0300

    Finalizacao dos testes na tela de login e inicio do componente Table refernte aos requisitos 6,7,8

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
-// teste
+import PropTypes from 'prop-types';
+import { v4 as uuid } from 'uuid';
+import { connect } from 'react-redux';
+import { eraseExpense, idEdit } from '../redux/actions';
+
+// referencia uuid https://www.geeksforgeeks.org/how-to-create-an-unique-id-in-reactjs/ //
 
 class Table extends Component {
-  render() {
-    return (
-      <div>Table</div>
-    );
-  }
-}
+  handleDeleteBtn = (id) => {
+    const { expenses, deleteExpense } = this.props;
+    const newExpensesArray = expenses.filter((expense) => expense.id !== id);
+    deleteExpense(newExpensesArray);
+  };
 
-export default Table;
+  handleEditBtn = (id) => {
+    const { editingId } = this.props;
+    editingId(id);
+  };
+
+  render() {
+    const { expenses } = this.props;
+    return (
+      <table>
+        <tr>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
+        </tr>
+        <tbody>
+    
